2ba9a15c431dcd76f57b74fa38e73a6b7a5b3df9
David Calhoun <dpcalhoun@gmail.com>
2018-03-21 20:18:46 -0500

    [add] Additional broken route examples

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,16 @@
+import Router from 'next/router';
 import Link from 'next/link';
 import { Fragment } from 'react';
+
+const handleClick = e => {
+  e.preventDefault();
+  Router.push(e.target.href);
+};
 
 export default props => (
   <Fragment>
     <nav>
+      <h2>Working</h2>
       <Link href="/">
         <a>Index Page</a>
       </Link>{' '}
@@ -12,18 +19,25 @@ export default props => (
         <a>Static Page</a>
       </Link>{' '}
       —{' '}
+      <Link href="/dynamic-page?token=bar" as="/dynamic-page/bar">
+        <a>Dynamic Page: bar (w/ as)</a>
+      </Link>
+    </nav>
+    <nav>
+      <h2>Broken</h2>
       <Link href="/static-page/nested">
         <a>Static Page Nested</a>
       </Link>{' '}
       —{' '}
       <Link href="/dynamic-page/foo">
-        <a>Dynamic Page: foo</a>
+        <a>Dynamic Page: foo (w/o as)</a>
       </Link>{' '}
       —{' '}
-      <Link href="/dynamic-page/bar">
-        <a>Dynamic Page: bar</a>
-      </Link>
+      <a onClick={handleClick} href="/dynamic-page/baz">
+        Dynamic Page: baz (imperatively)
+      </a>
     </nav>
+    <hr />
     <h1>{props.children}</h1>
   </Fragment>
 );
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const pathMatch = require('path-match');
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
+const port = process.env.PORT || 3000;
 
 app.prepare().then(() => {
   createServer((req, res) => {
@@ -24,8 +25,8 @@ app.prepare().then(() => {
     } else {
       handle(req, res, parsedUrl);
     }
-  }).listen(3000, err => {
+  }).listen(port, err => {
     if (err) throw err;
-    console.log('> Ready on http://localhost:3000');
+    console.log(`> Ready on http://localhost:${port}`);
   });
 });
